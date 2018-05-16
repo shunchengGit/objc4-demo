747918f486cd7c0b073ae7eb88974a049149fc42
chengshun <chengshun@meituan.com>
2018-05-16 12:03:19 +0800

    CreateProject

---
+++ objc4/objc4-723/Build/Intermediates.noindex/objc.build/Debug/objc.build/Objects-normal/x86_64/List.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/shuncheng/Downloads/objc4-723/objc4/objc4-723/runtime/OldClasses.subproj/List.m
---
+++ objc4/objc4-723/Build/Intermediates.noindex/objc.build/Debug/objc.build/Objects-normal/x86_64/a1a2-blocktramps-arm64.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/shuncheng/Downloads/objc4-723/objc4/objc4-723/runtime/a1a2-blocktramps-arm64.s
